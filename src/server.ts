65c590b81e371655df908ddedbad638912eadafc
Caio Vinicius <caioviniciuscv18@gmail.com>
2021-06-24 09:16:09 -0300

    Aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,13 @@
+import "reflect-metadata";
 import express from "express";
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
 
-/**
- * GET => buscar informação
- * POST => inserir(criar) uma informação
- * PUT => alterar uma informação
- * DELETE => remover um dado
- * PATCH => alterar um informação específica
- */
-app.get("/test", (request, response) => {
-  //request => entrando
-  //response => saindo
-  return response.send("Olá NLW");
-})
+app.use(express.json());
 
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW método POST");
-});
+app.use(router);
 
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
